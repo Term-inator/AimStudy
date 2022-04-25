11d71543e85e6c060aae26cc3a95c35cbd7e95cd
Term-inator <1121745627@qq.com>
2022-04-25 23:35:23 +0800

    routers

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,16 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import LoginView from "../views/login/login.vue"
+import MainView from '../views/mainPage/mainPage.vue'
+import LoginView from '../views/login/login.vue'
+import CourseQueryView from '../views/courseQuery/courseQuery.vue'
+import CourseTableView from '../views/courseTable/courseTable.vue'
+
+import CourseScoreView from '../views/eduAdmin/courseScore/courseScore.vue'
+import OpenCourseManagementView from '../views/eduAdmin/openCourseManagement/openCourseManagement.vue'
+import SelectCourseManagementView from '../views/eduAdmin/selectCourseManagement/selectCourseManagement.vue'
+import StudentInfoView from '../views/eduAdmin/studentInfo/studentInfo.vue'
+import StudentScoreView from '../views/eduAdmin/studentScore/studentScore.vue'
+import TeacherInfoView from '../views/eduAdmin/teacherInfo/teacherInfo.vue'
 
 export const constantRoutes = [
   {
@@ -12,19 +22,71 @@ export const constantRoutes = [
     path: "/login",
     name: "login",
     component: LoginView
+  },
+  {
+    path: '/main',
+    name: 'main',
+    component: MainView,
+  },
+  {
+    path: "/courseQuery",
+    name: "courseQuery",
+    component: CourseQueryView
+  },
+  {
+    path: "/courseTable",
+    name: "courseTable",
+    component: CourseTableView
   }
 ]
 
 export const asyncRoutes = [
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+    path: '/courseScore',
+    name: 'courseScore',
+    component: CourseScoreView,
     meta: {
-      roles: ['admin', 'edu_admin', 'teacher']
+      roles: ['edu_admin']
+    }
+  },
+  {
+    path: '/openCorseManagement',
+    name: 'openCorseManagement',
+    component: OpenCourseManagementView,
+    meta: {
+      roles: ['edu_admin']
+    }
+  },
+  {
+    path: '/selectCorseManagement',
+    name: 'selectCorseManagement',
+    component: SelectCourseManagementView,
+    meta: {
+      roles: ['edu_admin']
+    }
+  },
+  {
+    path: '/studentInfo',
+    name: 'studentInfo',
+    component: StudentInfoView,
+    meta: {
+      roles: ['edu_admin']
+    }
+  },
+  {
+    path: '/studentScore',
+    name: 'studentScore',
+    component: StudentScoreView,
+    meta: {
+      roles: ['edu_admin']
+    }
+  },
+  {
+    path: '/teacherInfo',
+    name: 'teacherInfo',
+    component: TeacherInfoView,
+    meta: {
+      roles: ['edu_admin']
     }
   }
 ]
---
+++ src/views/courseTable/courseTable.vue
@@ -0,0 +1,17 @@
+<template>
+  course table
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { onMounted } from '@vue/runtime-core'
+
+export default defineComponent({
+  name: "CourseTableView",
+  setup() {
+    onMounted(() => {
+      console.log('course table')
+    })
+  },
+})
+</script>
