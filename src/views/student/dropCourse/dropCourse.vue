e7b1703dde0bc966e5d26aa83b46066e8ac405a0
Term-inator <1121745627@qq.com>
2022-05-16 23:28:40 +0800

    ✨ feat(views/student/dropCourse): 学生退课

--- src/views/student/dropCourse/dropCourse.vue
+++ src/views/student/dropCourse/dropCourse.vue
@@ -1,17 +1,133 @@
 <template>
-  drop course
+  <div class="main">
+    <div class="search">
+      <search-form :items="search_form"></search-form>
+    </div>
+    <div class="course-list">
+      <a-table :columns="columns" :data-source="courses" size="small" :pagination="false" bordered>
+        <template #bodyCell="{ column, record }">
+          <template v-if="column.dataIndex === 'action'">
+            <a-button type="link" size="small" @click="drop(record.key)">退课</a-button>
+          </template>
+        </template>
+      </a-table>
+    </div>
+  </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
-import { onMounted } from '@vue/runtime-core'
+import { defineComponent, ref } from 'vue'
+import SearchForm from '@/components/searchForm/searchForm.vue'
+
+const search_form = [
+  {
+    title: "学年学期",
+    type: "select", // TODO cascade select
+    rules: {
+      required: false
+    }
+  }
+]
+
+const columns = [
+  {
+    title: '序号',
+    dataIndex: 'key',
+    key: 'key',
+    width: 30
+  },
+  {
+    title: '课程序号',
+    dataIndex: 'index',
+    key: 'index',
+    width: 100
+  },
+  {
+    title: '课程名称',
+    dataIndex: 'name',
+    key: 'name',
+    width: 120
+  },
+  {
+    title: '课程类型',
+    dataIndex: 'type',
+    key: 'type',
+    width: 100
+  },
+  {
+    title: '开课院系',
+    dataIndex: 'department',
+    key: 'department',
+    width: 100
+  },
+  {
+    title: '教师',
+    dataIndex: 'teacher',
+    key: 'teacher',
+    width: 80
+  },
+  {
+    title: '学分',
+    dataIndex: 'credit',
+    key: 'credit',
+    width: 80
+  },
+  {
+    title: '操作',
+    dataIndex: 'action',
+    key: 'outline',
+    width: 30
+  }
+]
 
 export default defineComponent({
   name: "DropCourseView",
+  components: {
+    SearchForm
+  },
   setup() {
-    onMounted(() => {
-      console.log('drop course')
-    })
+    const courses = ref(
+      [...Array(15)].map((_, i) => ({
+        key: i,
+        index: '1',
+        name: `计算机网络${i}`,
+        type: '专业必修',
+        department: '计算机学院',
+        teacher: '张三',
+        credit: '3.0',
+        }
+      )))
+
+    const drop = (key) => {
+      // TODO 退课
+      console.log(key)
+    }
+
+    return {
+      search_form,
+      columns,
+      courses,
+      drop
+    }
   },
 })
 </script>
+
+<style scoped>
+  .main {
+    padding: 35px 50px 0 50px;
+  }
+
+  .search {
+    padding: 0 0 10px 0;
+  }
+
+  .course-list {
+    padding: 30px 0 0 0;
+  }
+
+  ::v-deep .ant-table-cell ,.table-cell-button-font{
+    font-size: 5px;
+    text-align: center;
+  }
+</style>
