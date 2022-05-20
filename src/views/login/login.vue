e0a66af4192af06711713333a512d1a54285bf00
Term-inator <1121745627@qq.com>
2022-05-20 12:17:39 +0800

    ✨ feat(login): 居中 & 背景

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="main">
-    <a-card title="登录" :bordered="false" class="login">
+    <a-card title="登录" class="login" bordered>
       <a-form
         :model="formState"
         name="basic"
@@ -11,23 +11,23 @@
         @finishFailed="onFinishFailed"
       >
         <a-form-item
-          label="Username"
+          label="用户名"
           name="username"
-          :rules="[{ required: true, message: 'Please input your username!' }]"
+          :rules="[{ required: true, message: '请输入用户名!' }]"
         >
           <a-input v-model:value="formState.username" />
         </a-form-item>
 
         <a-form-item
-          label="Password"
+          label="密码"
           name="password"
-          :rules="[{ required: true, message: 'Please input your password!' }]"
+          :rules="[{ required: true, message: '请输入密码' }]"
         >
           <a-input-password v-model:value="formState.password" />
         </a-form-item>
 
         <a-form-item :wrapper-col="{ offset: 8, span: 16 }">
-          <a-button type="primary" html-type="submit">Submit</a-button>
+          <a-button type="primary" html-type="submit">登录</a-button>
         </a-form-item>
       </a-form>
     </a-card>
@@ -72,11 +72,17 @@ export default defineComponent({
 </script>
 
 <style scoped>
- .main {
-   height: 100vh;
- }
+  .main {
+    display: flex;
+    height: 100vh;
+    background: url(@/assets/img/background3280x2160.png) no-repeat;
+    background-size: 100% 100%;
+    align-items: center;
+    justify-content: center;
+  }
 
- .login {
-   transform: translateY(50%);
- }
+  .login {
+    width: 40vw;
+    height: 40vh;
+  }
 </style>
