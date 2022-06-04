120b494757dabf5b4c8d61986fccf00884f780fc
Term-inator <1121745627@qq.com>
2022-06-04 15:27:43 +0800

    ✨ feat: adminManagement组件
    CRUD Table for admin

---
+++ src/components/adminManagement/adminManagement.vue
@@ -0,0 +1,216 @@
+<template>
+  <div class="main">
+    <h1> {{ title }} </h1>
+    <div class="search">
+      <search-form :items="search_form" @conditions="getConditions"></search-form>
+    </div>
+    <div class="tool-bar">
+      <a-button type="primary" @click="add" size="small" style="width: 100px;">新建</a-button>
+      <a-button @click="remove" size="small" style="width: 100px">删除</a-button>
+    </div>
+    <a-table 
+      :columns="columns" 
+      :data-source="data_source" 
+      :row-selection="{ selectedRowKeys: selectedRowKeys, onChange: onSelectChange }" 
+      size="small" bordered>
+      <template #bodyCell="{ column, text, record }">
+        <template v-if="column.dataIndex === 'key'">
+          {{ text }}
+        </template>
+        <template v-else-if="column.dataIndex === 'action'">
+          <span v-if="editableData[record.key]">
+            <a-popconfirm title="确认保存?" okText="确认" cancelText="取消" @confirm="save(record.key)">
+              <a-button type="link" size="small">保存</a-button>
+            </a-popconfirm>
+            <a-button type="link" @click="cancel(record.key)" size="small">取消</a-button>
+          </span>
+          <span v-else>
+            <a-button type="link" @click="edit(record.key)" size="small">修改</a-button>
+          </span>
+        </template>
+        <template v-else>
+          <div>
+            <a-input
+              v-if="editableData[record.key]"
+              v-model:value="editableData[record.key][column.dataIndex]"
+              size="small"
+            />
+            <template v-else>
+              {{ text }}
+            </template>
+          </div>
+        </template>
+      </template>
+    </a-table>
+    <!--新建-->
+    <a-modal v-model:visible="add_visible" title="新建课程" @ok="addOkHandle" centered>
+      <template #footer>
+        <a-button key="cancel" @click="addCancelHandle">取消</a-button>
+        <a-button key="submit" type="primary" :loading="add_loading" @click="addOkHandle">提交</a-button>
+      </template>
+      <a-form ref="formRef" :model="formState" :label-col="{ span: 4 }" :wrapper-col="{ span: 16 }">
+        <a-form-item v-for="(item, index) in add_modal" :key="index" :label="item.title" :name="item.name">
+          <a-input v-model:value="formState[item.name]" size="small" />
+        </a-form-item>
+      </a-form>
+    </a-modal>
+  </div>
+</template>
+
+<script>
+import { defineComponent, reactive, ref, toRaw } from 'vue'
+import SearchForm from '@/components/searchForm/searchForm.vue'
+import { cloneDeep } from 'lodash-es';
+
+
+export default defineComponent({
+  name: "AdminManagement",
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    search_form: {
+      type: Array,
+      required: true
+    },
+    columns: {
+      type: Array,
+      required: true
+    },
+    data_source: {
+      type: Array,
+      required: true
+    },
+    add_api: {
+      type: String,
+      required: true
+    },
+    remove_api: {
+      type: String,
+      required: true
+    },
+    update_api: {
+      type: String,
+      required: true
+    },
+    add_modal: {
+      type: Array,
+      required: true
+    }
+  },
+  components: {
+    SearchForm
+  },
+  setup(props) {
+    const state = reactive({
+      selectedRowKeys: []
+    })
+
+    const onSelectChange = selectedRowKeys => {
+      console.log('selectedRowKeys changed: ', selectedRowKeys)
+      state.selectedRowKeys = selectedRowKeys
+    }
+
+    // Modal add
+    const formRef = ref()
+    const formState = reactive({})
+
+    const add_visible = ref(false)
+    const add_loading = ref(false)
+
+    const addOkHandle = () => {
+      add_loading.value = true
+      setTimeout(() => {
+        formRef.value.validateFields().then(values => {
+          console.log('Received values of form: ', values)
+          console.log('formState: ', toRaw(formState))
+
+          const formData = new FormData()
+          for(const prop in formState) {
+            formData.append(prop, formState[prop])
+          }
+
+          // TODO 提交 api
+
+          add_loading.value = false
+          add_visible.value = false
+          formRef.value.resetFields()
+          console.log('reset formState: ', toRaw(formState))
+        }).catch(info => {
+          console.log('Validate Failed:', info)
+        });
+      }, 2000)
+    }
+
+    const addCancelHandle = () => {
+      add_visible.value = false
+    }
+
+    const add = () => {
+      add_visible.value = true
+    }
+
+    const remove = () => {
+      console.log('selectedRowKeys: ', state.selectedRowKeys)
+    }
+
+    const editableData = reactive({})
+
+    const edit = key => {
+      editableData[key] = cloneDeep(props.data_source.filter(item => key === item.key)[0])
+    }
+
+    const save = key => {
+      Object.assign(props.data_source.filter(item => key === item.key)[0], editableData[key])
+      delete editableData[key]
+    }
+
+    const cancel = key => {
+      delete editableData[key]
+    }
+
+    return {
+      state,
+      onSelectChange,
+      add,
+      remove,
+      edit, save, cancel,
+
+      editableData,
+
+      formRef,
+      formState,
+
+      add_visible,
+      add_loading,
+      addOkHandle,
+      addCancelHandle
+    }
+  },
+})
+</script>
+
+<style scoped>
+  .main {
+    padding: 20px 15px 0 15px;
+  }
+
+  h1 {
+    font-size: 16px;
+    font-weight: 500;
+  }
+
+  .search {
+    padding: 0 0 10px 0;
+  }
+
+  .tool-bar {
+    margin: 0 0 15px 0;
+  }
+
+  ::v-deep .ant-table-cell ,.table-cell-button-font{
+    font-size: 5px;
+    text-align: center;
+  }
+</style>
--- src/views/admin/studentManagement/studentManagement.vue
+++ src/views/admin/studentManagement/studentManagement.vue
@@ -1,17 +1,134 @@
 <template>
-  student management
+  <admin-management 
+    :title="'学生管理'" 
+    :search_form="search_form"
+    :columns="columns"
+    :data_source="students"
+    :add_api="add_api"
+    :remove_api="remove_api"
+    :update_api="update_api"
+    :add_modal="add_modal"
+    >
+  </admin-management>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
-import { onMounted } from '@vue/runtime-core'
+import { defineComponent, ref } from 'vue'
+import AdminManagement from '@/components/adminManagement/adminManagement.vue'
+
+const search_form = [
+  {
+    title: "姓名",
+    type: "input",
+    rules: {
+      required: false
+    }
+  },
+  {
+    title: "学号",
+    type: "input",
+    rules: {
+      required: false
+    }
+  },
+  {
+    title: "专业",
+    type: "select",
+    rules: {
+      required: false
+    }
+  },
+  {
+    title: "入学年份",
+    type: "input",
+    rules: {
+      required: false
+    }
+  }
+]
+
+const columns = [
+  {
+    title: '序号',
+    dataIndex: 'key',
+    key: 'key',
+    width: 30,
+  },
+  {
+    title: '学号',
+    dataIndex: 'student_id',
+    key: 'student_id',
+    width: 120
+  },
+  {
+    title: '姓名',
+    dataIndex: 'name',
+    key: 'name',
+    width: 100
+  },
+  {
+    title: '专业',
+    dataIndex: 'major',
+    key: 'major',
+    width: 100
+  },
+  {
+    title: '入学年份',
+    dataIndex: 'year',
+    key: 'year',
+    width: 100
+  },
+  {
+    title: '联系电话',
+    dataIndex: 'phone',
+    key: 'phone',
+    width: 100
+  },
+  {
+    title: '操作',
+    dataIndex: 'action',
+    key: 'action',
+    width: 60
+  }
+]
+
+const add_modal = [
+  {
+    title: '姓名',
+    name: 'name'
+  },
+  {
+    title: '专业',
+    name: 'major'
+  },
+  {
+    title: '联系电话',
+    name: 'phone'
+  }
+]
 
 export default defineComponent({
   name: "StudentManagementView",
+  components: {
+    AdminManagement
+  },
   setup() {
-    onMounted(() => {
-      console.log('student management')
-    })
+    const students = ref(
+      [...Array(15)].map((_, i) => ({
+        key: i,
+        student_id: '20180001',
+        name: `学生${i}`,
+        major: '计算机科学与技术',
+        year: '2019',
+        phone: '123456789'
+        }
+      )))
+    return {
+      search_form,
+      columns,
+      students,
+      add_modal
+    }
   },
 })
 </script>
