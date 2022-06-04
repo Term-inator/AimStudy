3d20414316940d2e14c8dc03dc89d7f6b4de084d
Term-inator <1121745627@qq.com>
2022-06-04 15:49:56 +0800

    ✨ feat: admin管理专业信息

--- src/views/admin/majorManagement/majorManagement.vue
+++ src/views/admin/majorManagement/majorManagement.vue
@@ -1,17 +1,84 @@
 <template>
-  major management
+  <admin-management 
+    :title="'专业管理'" 
+    :search_form="search_form"
+    :columns="columns"
+    :data_source="majors"
+    :add_api="add_api"
+    :remove_api="remove_api"
+    :update_api="update_api"
+    :add_modal="add_modal"
+    >
+  </admin-management>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
-import { onMounted } from '@vue/runtime-core'
+import { defineComponent, ref } from 'vue'
+import AdminManagement from '@/components/adminManagement/adminManagement.vue'
+
+const search_form = [
+  {
+    title: "名称",
+    type: "input",
+    rules: {
+      required: false
+    }
+  }
+]
+
+const columns = [
+  {
+    title: '序号',
+    dataIndex: 'key',
+    key: 'key',
+    width: 30,
+  },
+  {
+    title: '名称',
+    dataIndex: 'name',
+    key: 'name',
+    width: 100
+  },
+  {
+    title: '人数',
+    dataIndex: 'amount',
+    key: 'amount',
+    width: 100
+  },
+  {
+    title: '操作',
+    dataIndex: 'action',
+    key: 'action',
+    width: 60
+  }
+]
+
+const add_modal = [
+  {
+    title: '名称',
+    name: 'name'
+  }
+]
 
 export default defineComponent({
-  name: "MajorManagementView",
+  name: "TeacherManagementView",
+  components: {
+    AdminManagement
+  },
   setup() {
-    onMounted(() => {
-      console.log('major management')
-    })
+    const majors = ref(
+      [...Array(15)].map((_, i) => ({
+        key: i,
+        name: `专业${i}`,
+        amount: '20',
+        }
+      )))
+    return {
+      search_form,
+      columns,
+      majors,
+      add_modal
+    }
   },
 })
 </script>
